f3c8dc3386147b3723e6803a959ce5376ea963c2
Emanuele Spadaro <emanuelespadaro1@gmail.com>
2024-05-22 20:10:41 +0200

    chore: popup change

--- popup.tsx
+++ popup.tsx
@@ -1,23 +1,16 @@
 import { useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
     <div
       style={{
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
+      <p style={{textAlign: 'center'}}>
+        Subito Not Sold
+      </p>
+      <a href="https://github.com/EmanueleSpadaro/subito-not-sold" target="_blank">
+        GitHub Repository
       </a>
     </div>
   )
